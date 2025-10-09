671863405202974263757733b229e58ad64553ad
Omarkhaled34 <omarmae75@gmail.com>
2025-10-09 12:54:04 +0300

    Update rps_game.py

--- rps_game.py
+++ rps_game.py
@@ -1,4 +1,4 @@
-#python game : rock  paper  scissor
+#python game : rock  paper  scissor#python game : rock  paper  scissor
 import sys
 import random
 from enum import Enum
@@ -7,20 +7,24 @@ class RPS(Enum):
     rock = 1
     paper = 2
     scissor=3
-
-if choice<1 | choice>3:
-    sys.exit('you must enter a valid choice')
-python_choice=random.randint(1,3)
-print('you chose',str(RPS(choice).name))
-print('python chose',str(RPS(python_choice).name))
-if choice==1 and python_choice==2:
-    print('you won')
-elif choice==2 and python_choice==1:
-    print('you won')
-elif choice==3 and python_choice==2:
-    print('you won')
-elif choice==python_choice:
-    print('tie game')
-else:
-    print("python won")
-
+while True:
+    if choice<1 | choice>3:
+        sys.exit('you must enter a valid choice')
+    python_choice=random.randint(1,3)
+    print('you chose',str(RPS(choice).name))
+    print('python chose',str(RPS(python_choice).name))
+    if choice==1 and python_choice==2:
+        print('you won')
+    elif choice==2 and python_choice==1:
+        print('you won')
+    elif choice==3 and python_choice==2:
+        print('you won')
+    elif choice==python_choice:
+        print('tie game')
+    else:
+        print("python won")
+    playagain=input('play again? (y/n)')
+    if playagain=='y':
+        continue
+    else:
+        sys.exit(" you quit the game")
